8eb831cdab9bf833bfeae9b0b3f0ba072f21cd8f
DZ-2 <Fomin.vlad25@mail.ru>
2022-08-09 16:38:45 +0300

    1

---
+++ src/main/java/by/tms/web/servlet/RegisterServlet.java
@@ -0,0 +1,14 @@
+package by.tms.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/register")
+public class RegisterServlet {
+
+
+
+}
